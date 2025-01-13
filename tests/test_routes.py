72a6755f39434ebeff8dd613a91fa540b8dd0d67
zee2theodd <zee2theodd@github.com>
2025-01-13 17:00:37 -0500

    Added test case for listing accounts

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -128,3 +128,10 @@ class TestAccountService(TestCase):
         """It should not Read an Account that is not found"""
         resp = self.client.get(f"{BASE_URL}/0")  # Invalid account ID
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
+    def test_get_account_list(self):
+        """It should Get a list of Accounts"""
+        self._create_accounts(5)  # Create 5 test accounts
+        resp = self.client.get(BASE_URL)  # Send GET request to list all accounts
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(len(data), 5)  # Ensure 5 accounts are returned
