dfc6c739941460f6444397f712b6912c94cd4260
zee2theodd <zee2theodd@github.com>
2025-01-13 16:25:06 -0500

    Fixed indentation and verified tests for account not found

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -124,3 +124,7 @@ class TestAccountService(TestCase):
         self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
 
     # ADD YOUR TEST CASES HERE ...
+    def test_get_account_not_found(self):
+        """It should not Read an Account that is not found"""
+        resp = self.client.get(f"{BASE_URL}/0")  # Invalid account ID
+        self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
